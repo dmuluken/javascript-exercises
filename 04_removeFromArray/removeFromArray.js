0570fab4cbbcfda90388e25aae9bc439d81e282c
dmuluken <dmuluken@gmail.com>
2024-10-09 14:28:35 +0300

    Finished with excercise 04.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    let originalArray = arguments[0];
+    arrayLength = originalArray.length;
+    argLength = arguments.length;
+    let newArray = [];
+    console.log(arrayLength);
+    console.log(argLength);
+    for (i = 1; i < argLength; i++) {
+        newArray = [];
+        for (j = 0; j < arrayLength; j++) {
+            if (originalArray[j] === arguments[i]) {
+                continue;
+            }
+            newArray.push(originalArray[j]);
+        }
+        originalArray = newArray;
+        arrayLength = originalArray.length;
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
