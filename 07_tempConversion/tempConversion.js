60c1da11cbbb865c9820351d9d162dcc795c892c
dmuluken <dmuluken@gmail.com>
2024-10-09 16:07:45 +0300

    Finished with excercise 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempFahrenheit) {
+  tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+  if (tempCelsius != 0) {
+    tempCelsius = Number(tempCelsius.toFixed(1));
+  }
+  return tempCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempCelsius) {
+  tempFahrenheit = tempCelsius * 9 / 5 + 32;
+  if (tempFahrenheit != 0) {
+    tempFahrenheit = Number(tempFahrenheit.toFixed(1));
+  }
+  return tempFahrenheit;
 };
 
 // Do not edit below this line
