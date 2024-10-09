1f5adde19185aa9fcac67c88320cab6e25f77d36
dmuluken <dmuluken@gmail.com>
2024-10-09 11:03:18 +0300

    Finished with excercise 02.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+    let result = "";
+    if (num == 0) {
+        return "";
+    } else if (num < 0) {
+        return "ERROR";
+    } else if (str == "") {
+        return "";
+    } else {
+        for (let i = 0; i < num; i++) {
+            result += str;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
