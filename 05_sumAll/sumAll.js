1c4748ace6ea231876e7783bae8ac21340627388
dmuluken <dmuluken@gmail.com>
2024-10-09 15:18:34 +0300

    Finished with excercise 05.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function (initialNumber, finalNumber) {
+    let sum = 0;
+    if (typeof (initialNumber) !== "number" || typeof (finalNumber) !== "number" || initialNumber < 0 || finalNumber < 0 || !(Number.isInteger(initialNumber)) || !(Number.isInteger(finalNumber))) {
+        return "ERROR";
+    } else {
+        if (initialNumber >= 0 && finalNumber >= 0) {
+            if (initialNumber < finalNumber) {
+                for (i = initialNumber; i <= finalNumber; i++) {
+                    sum += i;
+                }
+            }
+            // console.log(sum);
+            else {
+                for (i = initialNumber; i >= finalNumber; i--) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
