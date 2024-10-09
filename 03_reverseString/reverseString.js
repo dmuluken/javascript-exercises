094439439a4d97a48c3fca6c7dd13b7d04caf37d
dmuluken <dmuluken@gmail.com>
2024-10-09 11:28:30 +0300

    Finished with excercise 03.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function (str) {
+    len = -1 * str.length;
+    // console.log(len);
+    // console.log(str.slice(-2, -1));
+    result = "";
+    if (len == 0) {
+        return result;
+    } else {
+        for (i = -1; i >= len; i--) {
+            if (i == -1) {
+                result += str.slice(i);
+            } else {
+                result += str.slice(i, i + 1);
+            }
+        }
+    }
 
+    return result;
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
