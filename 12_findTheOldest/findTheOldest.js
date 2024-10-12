0725b344bd56dd535cecb457cb3ebe36f5b415bb
dmuluken <dmuluken@gmail.com>
2024-10-13 02:12:37 +0300

    Finished with excercise 12.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    const oldest = people.sort(function (a, b) {
+        if (!(a.yearOfDeath)) {
+            a.yearOfDeath = new Date().getFullYear;
+        }
+        if (!(b.yearOfDeath)) {
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
