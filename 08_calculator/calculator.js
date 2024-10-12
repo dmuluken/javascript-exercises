1d27e51ca154ddee3b357ead9188159cb0a6f76a
dmuluken <dmuluken@gmail.com>
2024-10-12 20:28:44 +0300

    Finished with excercise 08.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  const sumAll = arr.reduce((total, currentNumber) => {
+    return total + currentNumber;
+  }, 0);
+  return sumAll;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  const multipyAll = arr.reduce((total, currentNumber) => {
+    return total * currentNumber;
+  }, 1);
+  return multipyAll;
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
