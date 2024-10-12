6aa17bf4a7b4663ad6dec71365e1e43a857bdcb5
dmuluken <dmuluken@gmail.com>
2024-10-13 01:28:30 +0300

    Finished with excercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    let fab1 = 0;
+    let fab2 = 1;
+    let fab;
+    num = Number(num);
+    if (num < 0) {
+        return 'OOPS';
+    } else if (num == 0) {
+        return 0;
+    } else if (num == 1) {
+        return 1;
+    } else {
+        for (i = 2; i <= num; i++) {
+            fab = fab2 + fab1;
+            fab1 = fab2;
+            fab2 = fab;
+        }
+        return fab;
+    }
 };
 
 // Do not edit below this line
